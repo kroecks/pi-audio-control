8e81e63df33049a09fe10683b1a4e917cc085d35
Ken Roecks <kroecks@gmail.com>
2025-09-28 21:57:08 -0700

    refactor attempt

--- main.py
+++ main.py
@@ -122,7 +122,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8080)run(
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+    run(
         ['bluetoothctl', 'pair', device.mac],
         capture_output=True,
         text=True,
